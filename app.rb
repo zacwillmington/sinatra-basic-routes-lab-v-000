68f4da4e33509d6a0f335869a2f4b496ac1c814c
Zac Willmington <zac.willmington@gmail.com>
2018-05-04 17:47:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+        "My name is Zac"
+    end
+
+    get "/hometown" do
+        "My hometown is \_\_"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is \_\_"
+    end
 end
